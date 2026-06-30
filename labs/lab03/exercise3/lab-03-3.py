98136a146c391a5ed59f07a178bfb9996f15b52f
alifaimanlawi08@gmail,com <alifaimanlawi@gmail.com>
2026-06-30 15:31:43 +0800

    exercise 3

---
+++ labs/lab03/exercise3/lab-03-3.py
@@ -0,0 +1,9 @@
+kilobyte = int(input())
+megabyte = float(kilobyte) / 1024
+gigabyte = float(megabyte) / 1024
+terabyte = float(gigabyte) / 1024
+petabyte = float(terabyte) / 1024
+print(megabyte)
+print(gigabyte)
+print(terabyte)
+print(petabyte)
